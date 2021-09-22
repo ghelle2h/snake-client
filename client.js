50e2ee7e27b6f07c8fd2a53cecd506e7f7ec0355
Hamza Ghelle <hamzaghellegmail.com>
2021-09-22 18:35:33 -0400

    establish connection with the board

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function (data) {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  if(data){
+    console.log("you ded cuz you idled")
+  }
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = connect;
