12be6ffbfa6d74a0c7b51a8252807f0a4a56873c
Hamza Ghelle <hamzaghellegmail.com>
2021-09-22 22:14:02 -0400

    use connection to move snake around

--- client.js
+++ client.js
@@ -1,22 +1,42 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function (data) {
+const connect = function () {
+  const name = 'Name: GH';
+  const up = 'Move: up';
+  const down = 'Move: down';
+  const right = 'Move: right';
+  const left = 'Move: left';
+
   const conn = net.createConnection({
     host: '135.23.223.133',
     port: 50542
   });
 
   // interpret incoming data as text
-  conn.setEncoding("utf8");
-  if(data){
-    console.log("you ded cuz you idled")
-  }
+  
 
+  conn.write(`${name}`);
+  /*
+  setTimeout(() => conn.write(`${up}`), 50);
+  setTimeout(() => conn.write(`${down}`), 300);
+  setTimeout(() => conn.write(`${right}`), 600);
+  setTimeout(() => conn.write(`${left}`), 900);
+  */
+  
+
+  conn.on('data', (data) => {
+    console.log("you ded cuz you idled", data)
+  })
+
+  conn.on('connect', () => {
+    console.log("You are connected");
+  })
+  conn.setEncoding("utf8");
+  
   return conn;
 };
 
-console.log("Connecting ...");
-connect();
 
-module.exports = connect;
+
+module.exports = {connect};
--- input.js
+++ input.js
@@ -1,22 +1,38 @@
-const  connect  = require("./client");
+let connection ;
 
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
-
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
-
+  stdin.on('data', handleUserInput);
   return stdin;
-};
+  
+  
+}
 
-const handleUserInput = function (input) {
+
+
+const handleUserInput = (input) => {
   // your code here
   if (input === '\u0003') {
     process.exit();
+  } else if (input === 'w') {
+    connection.write("Move: up");
+  } else if (input === 'a') {
+    connection.write("Move: left");
+  } else if (input === 's') {
+    connection.write("Move: down");
   }
-};
+  else if (input === 'd') {
+    connection.write("Move: right");
+  }
+  connection.write(`${input}`);
 
-module.exports = setupInput;
+  
+}
+ 
+
+module.exports = {setupInput};
